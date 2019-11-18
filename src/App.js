19e0680a479876ae8901300c7f27f563751c239c
John N. Norman <normanj777@gmail.com>
2019-11-18 11:00:47 -0500

    Start over

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css';
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jamming</h1>
+        
+      </div>
+    );
+  }
+}
+
